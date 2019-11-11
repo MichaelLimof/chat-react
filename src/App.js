6b9c2daae3dd778d721fe5db9077b84eafe0ac6d
unknown <michael.limof@gmail.com>
2019-11-10 21:26:31 -0300

    continuous

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+
+
+
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
+import Navbar from './components/Navbar';
+
+const theme = createMuiTheme({
+
+  palette: {
+    primary: {
+      light: '#72cff8',
+      main: '#2196f3',
+      dark: '#0d47a1',
+      contrastText: '#ffff'
+
+    }, secondary: {
+      light: '#c5cae9',
+      main: '#3f51b5',
+      dark: '#512da8',
+      contrastText: '#3f51b5'
+    }
+  }, typography: {
+    useNextVariants: true,
+  }
+
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <div className="container">
+            <Navbar />
+
+            <Switch>
+              <Route exact path="/home" component={home} />
+              <Route exact path="/login" component={login} />
+              <Route exact path="/signup" component={signup} />
+
+            </Switch>
+          </div>
+        </Router>
+      </div>
+
+    </MuiThemeProvider>
   );
 }
 
